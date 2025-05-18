9025318075c198a1152cb795af3a57b1f20eee36
ni9eball <peterkayode2018@gmail.com>
2025-05-18 20:21:17 +0100

    UI

--- src/routes/publicProduct.routes.ts
+++ src/routes/publicProduct.routes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { getProductById } from "../controllers/publicProduct.controller";
 const router = Router();
 // This route handles fetching a specific product by its ID for a specific plug.
-
-// Get all supplier products
 router.get("/:plugId/:productId", getProductById);
 
 export default router;
